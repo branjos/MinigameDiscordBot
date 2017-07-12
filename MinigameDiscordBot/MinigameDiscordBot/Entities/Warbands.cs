5df8dac38e40526d20880cc53715b10be7f84323
branjos <branjos0824@yahoo.com>
2017-07-12 16:33:50 -0400

    init warbands

--- MinigameDiscordBot/MinigameDiscordBot/Entities/Warbands.cs
+++ MinigameDiscordBot/MinigameDiscordBot/Entities/Warbands.cs
@@ -6,10 +6,40 @@ namespace MinigameDiscordBot.Entities
 {
     class Warbands
     {
-        List<int> dwfWorlds;
-        List<int> elmWorlds;
-        List<int> rdiWorlds;
+        List<WarbandsWorld> dwfWorlds;
+        List<WarbandsWorld> elmWorlds;
+        List<WarbandsWorld> rdiWorlds;
 
+        public Warbands()
+        {
+            dwfWorlds = new List<WarbandsWorld>();
+            elmWorlds = new List<WarbandsWorld>();
+            rdiWorlds = new List<WarbandsWorld>();
+        }
 
+        public string AddWorld(int num, string loc)
+        {
+            WarbandsWorld w = new WarbandsWorld(num);
+
+            if(loc == "dwf")
+            {
+                dwfWorlds.Add(w);
+                return "World " + num + " added as a " + loc + "world.";
+            }
+            else if(loc == "elm")
+            {
+                elmWorlds.Add(w);
+                return "World " + num + " added as a " + loc + "world.";
+            }
+            else if(loc == "rdi")
+            {
+                rdiWorlds.Add(w);
+                return "World " + num + " added as a " + loc + "world.";
+            }
+            else
+            {
+                return "Invalid location.";
+            }
+        }
     }
 }
---
+++ MinigameDiscordBot/MinigameDiscordBot/Entities/WarbandsWorld.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinigameDiscordBot.Entities
+{
+    class WarbandsWorld : IEquatable<WarbandsWorld>
+    {
+        public int WorldNum { get; }
+        public string Status { get; set; }
+        public bool IsFreeWorld { get; }
+        private int[] freeWorlds = { 3, 7, 8, 11, 17, 19, 20, 29, 33, 34, 38, 41, 43, 57, 61, 80,
+            81, 108, 120, 135, 136, 141, 13, 93, 94, 109, 110, 111, 112, 113, 125, 126, 127, 128,
+            129, 130, 131, 132, 133, 47, 55, 75, 101, 102, 118, 121, 122 };
+
+        public WarbandsWorld(int worldnum)
+        {
+            WorldNum = worldnum;
+            Status = "";
+            IsFreeWorld = false;
+            for (int i = 0; i < freeWorlds.Length; ++i)
+            {
+                if (freeWorlds[i] == WorldNum)
+                {
+                    IsFreeWorld = true;
+                    break;
+                }
+            }
+        }
+
+        public string UpdateStatus(string status)
+        {
+            if(status == "beaming")
+            {
+                Status = status;
+            } 
+            else if(status == "dead")
+            {
+                Status = status;
+            }
+            else if(status == "broken")
+            {
+                Status = status;
+            }
+            else
+            {
+                return "That status is not valid.";
+            }
+            return "Status updated.";
+        }
+
+        public bool Equals(WarbandsWorld other)
+        {
+            if (other == null) { return false; }
+            if (other.WorldNum == WorldNum) { return true; }
+            else { return false; }
+        }
+    }
+}
