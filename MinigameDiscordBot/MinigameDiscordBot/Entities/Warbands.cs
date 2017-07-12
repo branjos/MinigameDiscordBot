289db59cc4f82b6322ea18119ec0275edb9d8516
Brandon Smith <branjos0824@yahoo.com>
2017-07-11 21:45:21 -0400

    working on getting a warbands going

---
+++ MinigameDiscordBot/MinigameDiscordBot/Entities/Warbands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinigameDiscordBot.Entities
+{
+    class Warbands
+    {
+        List<int> dwfWorlds;
+        List<int> elmWorlds;
+        List<int> rdiWorlds;
+
+
+    }
+}
