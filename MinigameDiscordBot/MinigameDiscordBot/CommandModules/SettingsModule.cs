544b1349d089d147c39222ccee376b98d0104c7a
branjos <branjos0824@yahoo.com>
2017-07-13 14:34:42 -0400

    init settings module

---
+++ MinigameDiscordBot/MinigameDiscordBot/CommandModules/SettingsModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinigameDiscordBot.CommandModules
+{
+    [Group("settings")]
+    class SettingsModule : ModuleBase
+    {
+        [Command("warbandsserver")]
+        public async Task UpdateWarbandsServer(ulong id)
+        {
+
+        }
+    }
+}
